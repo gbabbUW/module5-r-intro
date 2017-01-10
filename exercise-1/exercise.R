12c32e89409ec6bc003bba45ec531cd608a87875
Gavin Babb <gbabb@uw.edu>
2017-01-10 14:45:09 -0800

    Finish excercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,23 +2,32 @@
 
 # Create a variable `hometown` that stores the city in which you were born
 
+hometown <- "Wyoming"
 
 # Assign your name to the variable `my.name`
 
+my.name <- "Carlos Danger"
 
 # Assign your height (in inches) to a variable `my.height`
 
+my.height <- 70
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
 
+puppies <- 525000000
 
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
 
+puppy.price <- 20
 
 # Create a variable `total.cost` that has the total cost of all of your puppies
 
+total.cost <- puppies * puppy.price
 
 # Create a boolean variable `too.expensive`, set to TRUE if the cost is greater than $1,000
 
+too.expensive <- TRUE
 
 # Create a variable `max.puppies`, which is the number of puppies you can afford for $1,000.
+
+max.puppies <- 1000 / puppy.price
